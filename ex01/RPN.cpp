f46f2bf2722345fffc7533448c3f20dd50a6c466
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2024-12-17 18:25:29 +0000

    fix: Changes the order of the operands.

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -24,16 +24,14 @@ bool	isNumber(std::string num) {
 	return (true);
 }
 
-void	calculate(std::stack<std::string> *nums, std::string op, double &res) {
-	if (nums->size() < 2) {
-		delete nums;
+void	calculate(std::stack<std::string> &nums, std::string op) {
+	if (nums.size() < 2)
 		throw std::runtime_error("Error");
-	}
-	double		a = 0, b = 0;
-	a = std::strtod(nums->top().c_str(), NULL);
-	nums->pop();
-	b = std::strtod(nums->top().c_str(), NULL);
-	nums->pop();
+	double		a = 0, b = 0, res = 0;
+	b = std::strtod(nums.top().c_str(), NULL);
+	nums.pop();
+	a = std::strtod(nums.top().c_str(), NULL);
+	nums.pop();
 
 	if (op == "+")
 		res = a + b;
@@ -47,46 +45,37 @@ void	calculate(std::stack<std::string> *nums, std::string op, double &res) {
 		res = 0;
 	else
 		res = static_cast<int>(res);
+	std::stringstream t;
+	std::string	s;
+
+	t << static_cast<int>(res);
+	t >> s;
+	nums.push(s);
 }
 
 void	parseRPN(std::string rpn) {
-	std::stack<std::string>	*nums = new std::stack<std::string>();
+	std::stack<std::string>	nums = std::stack<std::string>();
 	std::string				token;
 	std::string::size_type	pos = 0;
-	double					res = std::numeric_limits<double>::max();
 	std::stringstream		sstr;
 
-	if (!nums) {
-		std::cerr << "Error: memory allocation failed" << std::endl;
-		return ;
-	}
 	while ((pos) != std::string::npos) {
 		pos = rpn.find_first_of(" \t\r\v\n\f");
 		token = rpn.substr(0, pos);
 		rpn.erase(0, pos + 1);
 		if (isOnlyWhiteSpace(token) || token.empty())
 			continue ;
-		else if (isNumber(token) && nums)
-			nums->push(token);
-		else if (isOperator(token) && nums && nums->size() >= 1) {
-			if (res != std::numeric_limits<double>::max()) {
-				sstr << static_cast<int>(res);
-				nums->push(sstr.str());
-				sstr.str("");
-			}
-			calculate(nums, token, res);
+		else if (isNumber(token))
+			nums.push(token);
+		else if (isOperator(token) && nums.size() >= 1) {
+			calculate(nums, token);
 		}
-		else {
-			delete nums;
+		else
 			throw std::runtime_error("Error");
-		}
 	}
-	if (nums->size() != 0) {
-		delete nums;
+	if (nums.size() != 1)
 		throw std::runtime_error("Error");
-	}
 	else
-		std::cout << res << std::endl;
-	delete nums;
+		std::cout << nums.top() << std::endl;
 }
 
